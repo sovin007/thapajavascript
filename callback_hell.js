f4cc7b86aad35cb899521d59090181b46b4495b3
sovin <sovin.chaudhary007@gmail.com>
2022-07-11 21:40:52 +0530

    javascript-files

---
+++ callback_hell.js
@@ -0,0 +1,15 @@
+<script>
+const roll = [101,102,103];
+const roll_no = function()
+{
+   setTimeout(function(){
+
+    console.log(roll);
+
+   },2000)
+}
+
+roll_no();
+
+
+</script>
---
+++ dom.php
@@ -0,0 +1,12 @@
+<p class="pp">Mahindra</p>
+
+<p class="pp">Mahindra</p>
+
+<p id="pp">Scorpio</p>
+
+
+<script>
+
+document.write(document.getElementById("pp").value);
+
+</script>
